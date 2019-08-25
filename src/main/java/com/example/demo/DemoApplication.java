5d5a2f20a6155748fa526dedf2f92ec43ede5a09
guohaidong <guo2184@gmail.com>
2019-08-25 11:12:08 +0800

    test jpa

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,31 @@
 package com.example.demo;
 
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@RequestMapping("/hello")
-	public String hello() {
-		return "hello world";
+	@RequestMapping("/user")
+	public List<User> hello() {
+		User user = new User();
+		user.setUserName("demo");
+		user.setAddress("深圳");
+		userRepository.save(user);
+		return userRepository.findAll();
 	}
 }
