378885b1c5f7222afdfe77eb59f745b6a566e19a
guohaidong <guo2184@gmail.com>
2019-09-02 10:09:49 +0800

    test jta rest

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,12 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		BigDecimal bigDecimal = new BigDecimal(String.valueOf((Double)null));
+		System.out.println(bigDecimal);
 	}
 
 }
